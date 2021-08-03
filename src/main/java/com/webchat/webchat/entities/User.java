01a386bfd7b5d9b83ee966675c41f79607f48584
iamducthanh <ducthanh260801@gmail.com>
2021-08-03 14:34:59 +0700

    qq

--- src/main/java/com/webchat/webchat/entities/User.java
+++ src/main/java/com/webchat/webchat/entities/User.java
@@ -1,17 +1,32 @@
 package com.webchat.webchat.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class User {
     private String fullname;
     private String password;
+
+    public User(String fullname, String password) {
+        this.fullname = fullname;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     @Override
     public String toString() {
