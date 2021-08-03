04aa31992794128d702174179bdef25584dce78e
iamducthanh <ducthanh260801@gmail.com>
2021-08-03 11:02:35 +0700

    add context

--- src/main/java/com/webchat/webchat/controller/acount/LockAcountController.java
+++ src/main/java/com/webchat/webchat/controller/acount/LockAcountController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LockAcountController {
+
     @RequestMapping("/lock_acount")
     public String lockAcountPage(){
         return "views/acount/lockacount";
