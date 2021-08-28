89646fb226d8e442631a348b4713857c52dbc71b
iamducthanh <ducthanh260801@gmail.com>
2021-08-28 22:07:46 +0700

    TESST

--- src/main/java/com/webchat/webchat/controller/acount/LockAcountController.java
+++ src/main/java/com/webchat/webchat/controller/acount/LockAcountController.java
@@ -1,12 +1,31 @@
 package com.webchat.webchat.controller.acount;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.webchat.webchat.dto.ImageDto;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class LockAcountController {
     @GetMapping("/lock_acount")
     public String lockAcountPage() {
         return "views/acount/lockacount";
+    }
+
+    @PostMapping("/testImage")
+    @ResponseBody
+    public void readImage(String dataI) throws JsonProcessingException {
+        System.out.println("dataa: "+dataI);
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<String> listData = Arrays.asList(objectMapper.readValue(dataI, String[].class));
+        System.out.println(listData.size());
     }
 }
---
+++ src/main/java/com/webchat/webchat/dto/ImageDto.java
@@ -0,0 +1,7 @@
+package com.webchat.webchat.dto;
+
+import java.util.List;
+
+public class ImageDto {
+    List<String> imageFile;
+}
