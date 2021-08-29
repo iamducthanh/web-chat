b10eb35bad5a963a351fe190f73c9afb1df7f758
iamducthanh <ducthanh260801@gmail.com>
2021-08-29 10:22:44 +0700

    save

--- src/main/java/com/webchat/webchat/controller/acount/LockAcountController.java
+++ src/main/java/com/webchat/webchat/controller/acount/LockAcountController.java
@@ -1,17 +1,17 @@
 package com.webchat.webchat.controller.acount;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.webchat.webchat.dto.ImageDto;
-import org.json.JSONObject;
+import com.webchat.webchat.utils.UploadUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class LockAcountController {
@@ -20,12 +20,19 @@ public class LockAcountController {
         return "views/acount/lockacount";
     }
 
+    @Autowired
+    private UploadUtil uploadUtil;
+
     @PostMapping("/testImage")
     @ResponseBody
-    public void readImage(String dataI) throws JsonProcessingException {
-        System.out.println("dataa: "+dataI);
+    public void readImage(String dataI) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         List<String> listData = Arrays.asList(objectMapper.readValue(dataI, String[].class));
-        System.out.println(listData.size());
+        for(String data:listData){
+            UUID id = UUID.randomUUID();
+            String name = String.valueOf(id);
+            uploadUtil.saveFile(data.toString(), name);
+        }
+
     }
 }
--- src/main/java/com/webchat/webchat/utils/UploadUtil.java
+++ src/main/java/com/webchat/webchat/utils/UploadUtil.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.Part;
-import java.io.File;
-import java.io.IOException;
+import javax.xml.bind.DatatypeConverter;
+import java.io.*;
+import java.util.UUID;
 
 @Component
 public class UploadUtil {
@@ -22,5 +23,32 @@ public class UploadUtil {
         File fileup = new File(dir, file.getSubmittedFileName());
         System.out.println(fileup.getAbsolutePath());
         file.write(fileup.getAbsolutePath());
+    }
+
+    public void saveFile(String dataString, String name){
+        String[] strings = dataString.split(",");
+        String extension;
+        switch (strings[0]) {
+            case "data:image/jpeg;base64":
+                extension = "jpeg";
+                break;
+            case "data:image/gif;base64":
+                extension = "gif";
+                break;
+            case "data:image/png;base64":
+                extension = "png";
+                break;
+            default:
+                extension = "jpg";
+                break;
+        }
+        byte[] data = DatatypeConverter.parseBase64Binary(strings[1]);
+        String path = "D:\\Temp\\" + name + "." + extension;
+        File file = new File(path);
+        try (OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(file))) {
+            outputStream.write(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
