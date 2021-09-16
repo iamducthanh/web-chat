5a59b8fc6ccd3a28b10fee83aebb4d6bc02f58ab
iamducthanh <ducthanh260801@gmail.com>
2021-09-16 21:48:44 +0700

    wait

---
+++ src/main/java/com/webchat/webchat/constant/CodeComfirm.java
@@ -0,0 +1,7 @@
+package com.webchat.webchat.constant;
+
+import java.util.HashMap;
+
+public class CodeComfirm {
+    public static HashMap<String, Integer> codeComfirm = new HashMap<>();
+}
--- src/main/java/com/webchat/webchat/controller/acount/SignUpController.java
+++ src/main/java/com/webchat/webchat/controller/acount/SignUpController.java
@@ -5,10 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -45,5 +42,12 @@ public class SignUpController {
             }
         }
         return error;
+    }
+
+    @PostMapping("/get-code")
+    @ResponseBody
+    public String getCode(@RequestParam("email") String email){
+        System.out.println(email);
+        return "";
     }
 }
